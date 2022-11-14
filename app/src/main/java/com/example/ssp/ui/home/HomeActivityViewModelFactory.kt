81cf641b87ab79746f57ee7c4c174508279958e2
Victor Ayala <victor.ayala2a@gmail.com>
2022-11-14 18:03:56 -0300

    List reservations finished

--- app/src/main/java/com/example/ssp/ui/home/HomeActivityViewModelFactory.kt
+++ app/src/main/java/com/example/ssp/ui/home/HomeActivityViewModelFactory.kt
@@ -1,5 +1,7 @@
 package com.example.ssp.ui.home
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.ssp.model.Person
@@ -7,6 +9,7 @@ import com.example.ssp.model.Person
 class HomeActivityViewModelFactory(
     private val account: Person
 ): ViewModelProvider.Factory {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if(modelClass.isAssignableFrom(HomeActivityViewModel::class.java)) {
             return HomeActivityViewModel(account) as T
