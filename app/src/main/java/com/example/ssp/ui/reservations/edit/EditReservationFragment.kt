4a99c9e8b6f558596f6bcf67165d29dc223c76b7
Victor Ayala <victor.ayala2a@gmail.com>
2022-11-15 07:30:00 -0300

    Edit reservation added

--- app/src/main/java/com/example/ssp/ui/reservations/edit/EditReservationFragment.kt
+++ app/src/main/java/com/example/ssp/ui/reservations/edit/EditReservationFragment.kt
@@ -1,12 +1,13 @@
 package com.example.ssp.ui.reservations.edit
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.ssp.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.ssp.databinding.FragmentEditReservationBinding
+import com.example.ssp.model.Reservation
 
 class EditReservationFragment : Fragment() {
 
@@ -15,18 +16,42 @@ class EditReservationFragment : Fragment() {
     }
 
     private lateinit var viewModel: EditReservationViewModel
+    private lateinit var binding: FragmentEditReservationBinding
+
+    private lateinit var reservation: Reservation
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_edit_reservation, container, false)
+    ): View {
+        this.binding = FragmentEditReservationBinding.inflate(inflater, container, false)
+
+        this.arguments?.let {
+            val args = EditReservationFragmentArgs.fromBundle(it)
+            this.reservation = args.reservation
+        }
+
+        return this.binding.root
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(EditReservationViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this)[EditReservationViewModel::class.java]
+
+        this.binding.buttonUpdate.setOnClickListener {
+            this.reservation.observacion = this.binding.editTextObservation.text.toString()
+            this.reservation.flagAsistio = if(this.binding.checkBoxAssisted.isChecked) {
+                "S"
+            } else {
+                "N"
+            }
+
+            viewModel.updateReservation(reservation, requireActivity())
+
+            activity?.onBackPressed()
+        }
+
     }
 
 }
--- app/src/main/java/com/example/ssp/ui/reservations/edit/EditReservationViewModel.kt
+++ app/src/main/java/com/example/ssp/ui/reservations/edit/EditReservationViewModel.kt
@@ -1,7 +1,44 @@
 package com.example.ssp.ui.reservations.edit
 
+import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
+import com.example.ssp.model.Person
+import com.example.ssp.model.Reservation
+import com.example.ssp.repository.ApiInterface
+import com.example.ssp.repository.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class EditReservationViewModel : ViewModel() {
+    fun updateReservation(reservation: Reservation, fragmentActivity: FragmentActivity) {
+        val retrofit = RetrofitClient.getInstance()
+        val apiInterface = retrofit.create(ApiInterface::class.java)
+        val call = apiInterface.putReservation(reservation)
 
+        call.enqueue(object : Callback<Reservation> {
+            override fun onResponse(call: Call<Reservation>, response: Response<Reservation>) {
+                if (response.isSuccessful) {
+                    //your code for handaling success response
+                    Toast.makeText(
+                        fragmentActivity,
+                        "Los datos de la reservación se actualizaron correctament",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                } else {
+                    Toast.makeText(
+                        fragmentActivity,
+                        "No se actualizó los datos \n Error: ${response.raw().code}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+            override fun onFailure(call: Call<Reservation>, t: Throwable) {
+                Toast.makeText(fragmentActivity, "Ocurrio un error", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
 }
